70f340ffceb77bc240a76e552579c6a4a35d89ff
maplepancakes <ngsiohongssg@gmail.com>
2021-05-10 19:32:03 +0800

    Modified createTask() function

--- src/models/createObject.js
+++ src/models/createObject.js
@@ -1,21 +1,24 @@
-import objectStorage from "./objectStorage";
+import dataStorage from "./dataStorage";
 
 const createObject = (function()
 {
     const createProject = function(projectName)
     {
-        objectStorage.projectObject[`${projectName}`] = [];
+        dataStorage.projectObject[`${projectName}`] = [];
 
-        objectStorage.consoleLogStorage();
+        dataStorage.consoleLogStorage();
     };
 
     const createTask = function(taskName, taskDescription, dueDate, priority, notes)
     {
+        this.taskID = dataStorage.taskID;
         this.taskName = taskName;
         this.taskDescription = taskDescription;
         this.dueDate = dueDate;
         this.priority = priority;
         this.notes = notes;
+
+        dataStorage.taskID++;
     };
 
     return {createProject, createTask};
