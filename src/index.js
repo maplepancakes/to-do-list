99560595e4f814a41e1335909136b214fd8f6cd5
maplepancakes <ngsiohongssg@gmail.com>
2021-05-12 22:45:22 +0800

    Checking of input

--- src/index.js
+++ src/index.js
@@ -1,45 +1,54 @@
 import createObject from "./models/createObject";
 import updateObject from "./models/updateObject";
-import validation from "./models/validation";
+import dataValidation from "./models/dataValidation";
 import page from "./views/page";
+import dataStorage from "./models/dataStorage";
 
-// Loads initial contents of page
-page.loadInitialContents();
+page.loadInitialContents(); // Loads initial contents of page
 
-// Button to open form that allows for addition of projects
-const openAddProjectFormButton = document.querySelector(`#new-project-button`);
+const openAddProjectFormButton = document.querySelector(`#new-project-button`); // Button to open form that allows for addition of projects
 
 openAddProjectFormButton.addEventListener(`click`, function(e)
 {
-    page.loadNewProjectForm();
-
-    // Button that adds user inputted projects in the textbox
-    const addProjectButton = document.querySelector(`#submit-new-project-button`);
-
-    addProjectButton.addEventListener(`click`, function(e)
+    const newProjectFormContainer = document.querySelector(`#new-project-form-container`);
+    
+    if (!newProjectFormContainer) // If the form that allows for addition of new projects is not loaded, run code block
     {
-        const newProjectInput = document.querySelector(`#new-project-input`);
+        page.loadNewProjectForm(); // Loads form that allows for addition of new projects
 
-        let isEmptyString = validation.validateIsEmptyString(newProjectInput.value);
+        const addProjectButton = document.querySelector(`#submit-new-project-button`); // Button that adds user inputted projects in the textbox
 
-        if (isEmptyString === true)
+        addProjectButton.addEventListener(`click`, function(e)
         {
-            page.updateAttribute(`#new-project-input`, `placeholder`, `Type something here...`);
-            
-            return;
-        }
-        else if (isEmptyString === false)
-        {
-            createObject.createProject(newProjectInput.value);
-        }
-        /* 
-        Requirements: -
-        - textbox input must not be empty
-        - if add project is clicked, form should only display once
-        - once [Add Project] is clicked, it should add the project
-            to storage, and also to display
-        */
-    });
+            const newProjectInput = document.querySelector(`#new-project-input`);
+
+            if (dataValidation.isEmptyString(newProjectInput.value) === false) // If textbox input in the project form is not empty, run code block
+            {
+                if (!dataStorage.getProjectObjectValue(newProjectInput.value)) // If textbox input does not contain an existing project name, run code block
+                {
+                    createObject.createProject(newProjectInput.value); // Creates new key-value pair, with key equal to value of textbox input, and the value an empty array []
+
+                    page.updateProjectListing(newProjectInput.value); // Appends textbox input to display
+                    page.updateAttribute(`#new-project-input`, `value`, ``); // Empties textbox value
+                    page.updateAttribute(`#new-project-input`, `placeholder`, `e.g. Daily Tasks`); // Updates textbox placeholder to 'e.g. Daily Tasks'
+                }
+                else if (dataStorage.getProjectObjectValue(newProjectInput.value)) // If textbox input contains an existing project name, prevent another project with the same name from being created
+                {
+                    return;
+                }
+            }
+            else if (dataValidation.isEmptyString(newProjectInput.value) === true) // If textbox input in the project form is empty, run code block
+            {
+                page.updateAttribute(`#new-project-input`, `placeholder`, `Type something here...`); // Updates textbox placeholder from 'e.g. Daily Tasks' to 'Type something here...'
+                
+                return;
+            }
+        });
+    }
+    else if (newProjectFormContainer) // If the form that allows for addition of new projects is already loaded, prevent another form from being loaded
+    {
+        return;
+    }
 });
 
 /*createObject.createProject(`Miscellaneous Tasks`);
